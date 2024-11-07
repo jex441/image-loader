ea0b31095aeb45b88c0531b7818a767773239280
Jeffrey Wood <jeffreywood.dev@gmail.com>
2024-11-07 11:26:47 -0500

    config removed

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-	/* config options here */
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "vastphotos.com",
-				port: "",
-				pathname: "/files/*",
-			},
-		],
-	},
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
